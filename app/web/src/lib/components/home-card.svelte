c4c55648776f1811cb674405cb88133a6133827a
FGates <fer_ssa94@hotmail.com>
2022-12-07 19:44:07 +0100

    component test

---
+++ app/web/src/lib/components/home-card.svelte
@@ -0,0 +1,9 @@
+<div class="hero bg-base-200">
+  <div class="hero-content flex-col lg:flex-row">
+    <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 class="text-5xl font-bold">Box Office News!</h1>
+      <p class="py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas hic illum accusamus laudantium non doloribus assumenda incidunt itaque! Eos ab iste autem molestias, praesentium cupiditate nulla ullam dolor! A, voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque reprehenderit non vitae explicabo accusamus expedita repellat quae aperiam. Laudantium natus dolores illo reprehenderit debitis iure magnam nulla rem nesciunt iusto.</p>
+    </div>
+  </div>
+</div>
--- app/web/src/routes/+page.svelte
+++ app/web/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import HomeCard from "$lib/components/home-card.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<HomeCard></HomeCard>
