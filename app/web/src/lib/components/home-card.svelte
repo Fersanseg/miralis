aceed83664a99d1b901d812d4835d7c245fd7e32
FGates <fer_ssa94@hotmail.com>
2022-12-08 16:27:37 +0100

    home card hover

--- app/web/src/lib/components/home-card.svelte
+++ app/web/src/lib/components/home-card.svelte
@@ -2,11 +2,12 @@
   export let data = { imgUrl: '', title: '', text: '', routeTo: '' }
 </script>
 
-<a href="{data.routeTo}" class="bg-base-300 rounded-2xl flex flex-col items-center">
+<a href="{data.routeTo}"
+   class="bg-base-300 rounded-2xl flex flex-col items-center
+    transition-all duration-200 ease-in-out hover:bg-black hover:bg-opacity-30 hover:scale-102">
   <img src="{data.imgUrl}" class="max-w-md rounded-2xl mt-12 mb-4" alt="libro.jpg"/>
   <div class="flex flex-col items-center">
     <h1 class="text-5xl font-bold text-center">{data.title}</h1>
     <p class="py-10 mx-16">{data.text}</p>
   </div>
 </a>
-    
