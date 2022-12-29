63ad7b7d5ed8ca016d4fce0ccb5776a1fe7da128
FGates <fer_ssa94@hotmail.com>
2022-12-29 19:13:11 +0100

    dev/prod pb url

--- app/web/src/hooks.server.ts
+++ app/web/src/hooks.server.ts
@@ -1,10 +1,12 @@
 import PocketBase from 'pocketbase';
 import type { Handle } from '@sveltejs/kit'
 import { Utils } from '$lib/utils';
-import { PB_LOCAL } from '$env/static/private';
+import { PB_LOCAL, PB_PROD } from '$env/static/private';
+import { dev } from '$app/environment';
 
 export const handle: Handle = async ({ event, resolve }) => {
-  event.locals.pb = new PocketBase(PB_LOCAL);
+  const url = dev ? PB_LOCAL : PB_PROD;
+  event.locals.pb = new PocketBase(url);
   event.locals.pb.authStore.loadFromCookie(event.request.headers.get("cookie") || "")
 
   if (event.locals.pb.authStore.isValid) {
