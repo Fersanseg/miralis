313f92aa2c50b098855eaa6ee921b725eb511e18
FGates <fer_ssa94@hotmail.com>
2023-02-02 19:30:18 +0100

    block monster-parts api route (shelved)

--- app/web/src/routes/api/monster-parts/+server.ts
+++ app/web/src/routes/api/monster-parts/+server.ts
@@ -1,13 +1,15 @@
-import type { CreaturesRecord } from "$lib/pocketbase-types";
+import type { CreaturesResponse } from "$lib/pocketbase-types";
 import type { RequestHandler } from "./$types";
 import { Utils } from "$lib/utils";
-import { json } from "@sveltejs/kit";
+import { error, json } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({locals}) => {
+  throw error(403, "Forbidden");
   try {
-    const dbData: Array<CreaturesRecord>  = await locals.pb.collection('creatures').getFullList(200, {sort: "name"});
+    const dbData: Array<CreaturesResponse>  = await locals.pb.collection('creatures').getFullList(200, {sort: "name"});
     const parsedData = dbData.map(item => {
       return {
+        "id": item.id,
         "name": item.name,
         "level": item.level,
         "properties": Utils.splitCreatureProperties(item.properties),
